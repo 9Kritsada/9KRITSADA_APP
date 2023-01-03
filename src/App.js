a3145ba640ea5f9df0d1808338b511000c7caa8d
9Kritsada <kritsada.wiriya@gmail.com>
2023-01-03 15:40:32 +0700

    1.2.2

--- src/App.js
+++ src/App.js
@@ -15,6 +15,11 @@ function App() {
       url: "https://www.instagram.com/9kritsada/",
       icon: "fa-brands fa-instagram",
     },
+    {
+      name: "mail",
+      url: "mailto:kritsada.wiriya@gmail.com",
+      icon: "fa-regular fa-envelope",
+    },
   ];
   const info = {
     title: "9KRITSADA",
@@ -23,31 +28,32 @@ function App() {
   const words = [{ title: "Student" }, { title: "Web Developer" }];
   // text-[#DCD6F7]
   return (
-    <div className="flex items-center justify-center h-screen font-['Kanit'] bg-gradient-to-tr from-[#7178AD] via-[#434975] to-[#1E2034] cursor-default selection:bg-[#a6b1e1] selection:text-[#ffffff]">
+    <div className="flex items-center justify-center h-screen font-['Kanit'] bg-gradient-to-tr from-[#7178AD] via-[#434975] to-[#1E2034] cursor-default selection:bg-[#a6b1e1] selection:text-[#ffffff] px-3">
       <Particle />
       <div className="text-center z-10">
-        <h1 className="md:text-6xl text-4xl font-bold mb-5 text-transparent bg-clip-text bg-gradient-to-r from-[#DCD6F7] to-[#CACFD6] tracking-[.05em] animate__animated animate__fadeInDown animate__delay-.5s  ">
+        <h1 className="md:text-6xl text-4xl font-bold mb-5 drop-shadow-lg text-[#DCD6F7] subpixel-antialiased tracking-[.05em] animate__animated animate__fadeInDown animate__delay-.5s  ">
           {info.title}
         </h1>
         <div>
-          <h1 className="md:text-3xl text-xl font-normal text-[#A6B1E1] animate__animated animate__fadeIn">
+          <h1 className="md:text-3xl text-xl font-normal text-[#A6B1E1] animate__animated animate__fadeIn drop-shadow-lg">
             <span>I'm a </span>
             <span>
               <Typewriter
                 words={words.map((e) => {
-                  return `${e.title}`;
+                  return `${e.title} `;
                 })}
                 loop={0}
                 cursor
-                cursorStyle="_"
-                typeSpeed={70}
-                deleteSpeed={50}
-                delaySpeed={2000}
+                cursorStyle="|"
+                typeSpeed={40}
+                deleteSpeed={40}
+                delaySpeed={5000}
               />
             </span>
           </h1>
         </div>
-        <div className="mt-5 py-5 border-t-[1px] border-[#CACFD6] animate__animated animate__fadeInUp animate__delay-1s">
+        <hr className="my-5 mx-auto w-56 h-1 bg-[#DCD6F7] rounded border-0 drop-shadow-lg"></hr>
+        <div className="flex flex-wrap animate__animated animate__fadeInUp animate__delay-1s">
           {links.map((e) => {
             return <Button name={e.name} link={e.url} icon={e.icon} />;
           })}
