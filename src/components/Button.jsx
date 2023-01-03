b49a18672454483523fc6ed0aa2ab364edd783d8
9Kritsada <kritsada.wiriya@gmail.com>
2023-01-03 19:25:37 +0700

    1.3.4

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 function Button(props) {
   const {name, link, icon} = props
   return (
-    <div className="min-[312px]:basis-auto basis-full mb-3">
+    <div className="min-[320px]:basis-auto basis-full mb-3">
       <a href={link} target="_blank" rel="noopener noreferrer" className=" cursor-pointer px-4 py-1 bg-[#DCD6F7] text-[#424874] mx-[0.25em] rounded drop-shadow-lg md:text-[14px] text-[12px] hover:opacity-80">
         <i class={`font-semibold ${icon}`}></i> {name.toUpperCase()}
       </a>
