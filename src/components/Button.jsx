56fac5cfd463c358e1cefe1d2a4c3f8b6c45738b
9Kritsada <kritsada.wiriya@gmail.com>
2023-02-12 21:15:03 +0700

    :bug: Fix : Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 function Button(props) {
   const {name, link, icon} = props
   return (
-    <div className="bg-[#DCD6F7] drop-shadow-lg hover:opacity-80 rounded px-2 py-1">
-      <a href={link} target="_blank" rel="noopener noreferrer" className=" text-[#424874] text-[12px] ">
+    <div className="bg-[#DCD6F7] drop-shadow-lg hover:opacity-80 rounded py-1 px-1  flex justify-center items-center">
+      <a href={link} target="_blank" rel="noopener noreferrer" className=" text-[#424874] text-[12px]">
         <i class={`font-semibold ${icon}`}></i> {name.toUpperCase()}
       </a>
     </div>
