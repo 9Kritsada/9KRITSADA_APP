a3b27797d7a29a8646427e9387755f515f1dcfe6
9Kritsada <kritsada.wiriya@gmail.com>
2023-02-15 01:16:40 +0700

    :sparkles: 1.5.2

--- src/App.jsx
+++ src/App.jsx
@@ -31,17 +31,17 @@ function App() {
   ];
   // text-[#DCD6F7]
   return (
-    <div className="flex items-center justify-center h-screen font-['Kanit'] bg-gradient-to-tr from-[#FCD9E9] via-[#F694C1] to-[#F58EBE] cursor-default selection:bg-[#EDE7B1] selection:text-[#000000] px-3">
+    <div className="flex items-center justify-center h-screen font-['Kanit'] bg-gradient-to-tr from-[#E8EDDF]  to-[#CFDBD5] cursor-default selection:bg-[#EDE7B1] selection:text-[#000000] px-3">
       <div className="text-center z-10">
-        <h1 className="min-[380px]:text-6xl min-[320px]:text-5xl text-4xl font-bold mb-5 drop-shadow-lg text-[#000000] subpixel-antialiased animate__animated animate__fadeInDown animate__delay-.5s  ">
+        <h1 className="min-[380px]:text-6xl min-[320px]:text-5xl text-4xl font-bold mb-5 drop-shadow-lg text-[#333533] subpixel-antialiased animate__animated animate__fadeInDown animate__delay-.5s  ">
           {info.title}
         </h1>
-        <div className="mx-auto w-80 rounded-full bg-[#EDE7B1] overflow-hidden  drop-shadow-md border-4 border-[#000000] hover:bg-[#eee273] transition duration-300 ease-in-out animate__animated animate__fadeIn">
+        <div className="mx-auto w-80 rounded-full bg-[#F5CB5C] overflow-hidden  drop-shadow-md border-4 border-[#333533] hover:bg-[#F0B719] transition duration-300 ease-in-out animate__animated animate__fadeIn">
           <img src={avatar} alt="Avatar" className=" drop-shadow-lg " />
         </div>
         <p className="text-purple-700 text-opacity-0">No one can be good at everything.</p>
         <div>
-          <h1 className="min-[480px]:text-3xl min-[320px]:text-2xl text-xl font-['Kalam'] font-thin text-[#000000] animate__animated animate__fadeIn">
+          <h1 className="min-[480px]:text-3xl min-[320px]:text-2xl text-xl font-['Kalam'] font-thin text-[#333533] animate__animated animate__fadeIn">
             <span>I'm a </span>
             
             <span>
@@ -59,7 +59,7 @@ function App() {
             </span>
           </h1>
         </div>
-        <hr className="my-5 mx-auto w-56 h-1 bg-[#000000] rounded border-0 px-5"></hr>
+        <hr className="my-5 mx-auto w-56 h-1 bg-[#242423] rounded border-0 px-5"></hr>
         <div className="grid gap-2 grid-cols-3 justify-center animate__animated animate__fadeInUp  mx-auto">
           {links.map((e) => {
             return <Button name={e.name} link={e.url} icon={e.icon} />;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 function Button(props) {
   const {name, link, icon} = props
   return (
-    <div className="bg-[#EDE7B1] drop-shadow-lg hover:bg-[#eee273] rounded py-1 px-1  flex justify-center items-center">
-      <a href={link} target="_blank" rel="noopener noreferrer" className=" text-[#000000] text-[12px]">
+    <div className="bg-[#F5CB5C] drop-shadow-lg hover:bg-[#F8DB8C] rounded py-1 px-1  flex justify-center items-center">
+      <a href={link} target="_blank" rel="noopener noreferrer" className=" text-[#242423] text-[12px]">
         <i class={`font-semibold ${icon}`}></i> {name.toUpperCase()}
       </a>
     </div>
