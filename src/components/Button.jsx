12fbb15a8432dd3f5b394aa3a10ff07e55b5449f
9Kritsada <kritsada.wiriya@gmail.com>
2023-02-18 00:35:17 +0700

    1.5.4

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 function Button(props) {
   const {name, link, icon} = props
   return (
-    <div className="bg-[#F5CB5C] drop-shadow-lg hover:bg-[#F8DB8C] rounded py-1 px-1  flex justify-center items-center">
+    <div className="bg-[#F5CB5C] drop-shadow-lg hover:bg-[#F0B719] rounded py-1 px-1  flex justify-center items-center">
       <a href={link} target="_blank" rel="noopener noreferrer" className=" text-[#242423] text-[12px]">
         <i class={`font-semibold ${icon}`}></i> {name.toUpperCase()}
       </a>
